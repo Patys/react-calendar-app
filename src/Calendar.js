9ebe869bc2761d16a6fa4205d1565446a97a08e0
Patryk Szczygło <patysprog@gmail.com>
2017-07-06 09:19:48 +0200

    init

---
+++ src/Calendar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Calendar extends React.Component {
+
+
+  render() {
+    return (
+      <div className="header">
+        <h2>Terminplaner</h2>
+        <div class="control">
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Calendar
